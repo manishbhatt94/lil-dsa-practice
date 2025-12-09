a841f758eb6b5187ec675532991c51450abc5ad7
Manish Bhatt <manishbhatt594@gmail.com>
2025-12-09 12:03:57 +0530

    LC80 - Add assertion for expected array correctness wrt size

--- src/test/java/manish/dsa/arrays/twopointers/LC80_RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayIITest.java
+++ src/test/java/manish/dsa/arrays/twopointers/LC80_RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayIITest.java
@@ -14,8 +14,10 @@ class RemoveDuplicatesFromSortedArrayIITest {
 	void testOne(ISolutionApproach_LC80 approach) {
 		int[] arr = { 1, 1, 1, 2, 2, 3 };
 		int actualK = approach.removeDuplicates(arr);
+
 		int expectedK = 5;
 		int[] expUpdatedArr = { 1, 1, 2, 2, 3 };
+		assertThat(expUpdatedArr).hasSize(expectedK);
 
 		assertThat(actualK).isEqualTo(expectedK);
 		assertThat(Arrays.copyOf(arr, actualK)).isEqualTo(expUpdatedArr);
@@ -26,8 +28,10 @@ class RemoveDuplicatesFromSortedArrayIITest {
 	void testTwo(ISolutionApproach_LC80 approach) {
 		int[] arr = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
 		int actualK = approach.removeDuplicates(arr);
+
 		int expectedK = 7;
 		int[] expUpdatedArr = { 0, 0, 1, 1, 2, 3, 3 };
+		assertThat(expUpdatedArr).hasSize(expectedK);
 
 		assertThat(actualK).isEqualTo(expectedK);
 		assertThat(Arrays.copyOf(arr, actualK)).isEqualTo(expUpdatedArr);
@@ -38,8 +42,10 @@ class RemoveDuplicatesFromSortedArrayIITest {
 	void testThree(ISolutionApproach_LC80 approach) {
 		int[] arr = { 1, 1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 6, 6, 6, 6, 7, 7, 8 };
 		int actualK = approach.removeDuplicates(arr);
+
 		int expectedK = 13;
 		int[] expUpdatedArr = { 1, 1, 2, 3, 3, 4, 4, 5, 6, 6, 7, 7, 8 };
+		assertThat(expUpdatedArr).hasSize(expectedK);
 
 		assertThat(actualK).isEqualTo(expectedK);
 		assertThat(Arrays.copyOf(arr, actualK)).isEqualTo(expUpdatedArr);
