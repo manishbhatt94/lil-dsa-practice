6e45248140d4e82ec7f4d867357cb49191f17df9
Manish Bhatt <manishbhatt594@gmail.com>
2025-12-25 14:55:13 +0530

    Add a couple more tests for LC75 DNF

--- src/test/java/manish/dsa/arrays/twopointers/LC75_SortColors/SortColorsTest.java
+++ src/test/java/manish/dsa/arrays/twopointers/LC75_SortColors/SortColorsTest.java
@@ -117,6 +117,18 @@ class SortColorsTest {
 				new TestCaseOutput()
 			),
 			new TestCase(
+				new TestCaseInput(new int[] { 0, 1, 2, 0, 1, 2 }),
+				new TestCaseOutput()
+			),
+			new TestCase(
+				new TestCaseInput(new int[] { 1, 0, 2, 1, 2, 0, 1 }),
+				new TestCaseOutput()
+			),
+			new TestCase(
+				new TestCaseInput(new int[] { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 }),
+				new TestCaseOutput()
+			),
+			new TestCase(
 				new TestCaseInput(new int[] { 2, 0, 1, 1, 2, 0, 0, 2, 0, 1, 0, 2, 1 }),
 				new TestCaseOutput()
 			)
